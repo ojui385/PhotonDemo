d26c70f2c22081751a1c6a6a4dbd5fa0e55f529d
Jumi Oh <ojui385@gmail.com>
2021-09-07 18:16:01 +0900

    Set Photon Nickname

--- Assets/02.Scripts/PhotonManager.cs
+++ Assets/02.Scripts/PhotonManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
     private readonly string gameVersion = "v1.0";
     private string userId = "Ojui";
+
+    public TMP_InputField userIdText;
+    public TMP_InputField roomNameText;
 
     private void Awake()
     {
@@ -24,29 +29,35 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     void Start()
     {
         Debug.Log("00. 포톤 매니저 시작");
+        userId = PlayerPrefs.GetString("USER_ID", $"USER_{Random.Range(0, 100):00}");
+        userIdText.text = userId;
         PhotonNetwork.NickName = userId;
     }
     public override void OnConnectedToMaster()
     {
         Debug.Log("01. 포톤 서버에 접속");
 
+        //로비에 접속
+        PhotonNetwork.JoinLobby();
+
     }
 
-    public void OnStartBtn()
+    public override void OnJoinedLobby()
     {
-        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("02. 로비에 접속");
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("02. 랜덤 룸 접속 실패");
+        Debug.Log("랜덤 룸 접속 실패");
 
         // 룸 속성 설정
         RoomOptions ro = new RoomOptions();
         ro.IsOpen = true;
         ro.IsVisible = true;
-        ro.MaxPlayers = 30;
+        ro.MaxPlayers = 2;
 
+        roomNameText.text = $"Room_{Random.Range(1, 100):000}";
 
         // 룸을 생성 > 자동 입장됨
         PhotonNetwork.CreateRoom("room_1", ro);
@@ -65,5 +76,24 @@ public class PhotonManager : MonoBehaviourPunCallbacks
             PhotonNetwork.LoadLevel("Level_1");
         }
     }
+
+    #region UI_BUTTON_CALLBACK
+    // Random 버튼 클릭
+    public void OnRandomBtn()
+    {
+        // ID 인풋필드가 비어있으면
+        if (string.IsNullOrEmpty(userIdText.text))
+        {
+            // 랜덤 아이디 부여
+            userId = $"USER_{Random.Range(0, 100):00}";
+            userIdText.text = userId;
+        }
+
+        PlayerPrefs.SetString("USER_ID", userIdText.text);
+        PhotonNetwork.NickName = userIdText.text;
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    #endregion
 
 }
